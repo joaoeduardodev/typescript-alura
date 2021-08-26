0c0fe4d7f1a893c874afe897a3901f20623f207e
João Eduardo <joaoeduardo980@gmail.com>
2021-08-25 22:00:08 -0300

    Modulo 4 - Avancando na modelagem do dominio

--- app/controllers/negociacao-controller.ts
+++ app/controllers/negociacao-controller.ts
@@ -1,8 +1,10 @@
+import { Negociacoes } from '../models/negociacoes.js';
 import { Negociacao } from './../models/negociacao.js';
 export class NegociacaoController {
     private inputData: HTMLInputElement;
     private inputQuantidade: HTMLInputElement;
     private inputValor: HTMLInputElement;
+    private negociacoes = new Negociacoes();
 
     constructor(){
         this.inputData = document.querySelector('#data');
@@ -12,7 +14,9 @@ export class NegociacaoController {
 
     adiciona(): void{
         const negociacao = this.criaNegociacao();
-        console.log(negociacao);
+        this.negociacoes.adiciona(negociacao);
+        this.negociacoes.lista();
+        console.log(this.negociacoes.lista());
         this.limparFormulario();
     }
 
---
+++ app/models/negociacoes.ts
@@ -0,0 +1,15 @@
+import { Negociacao } from "./negociacao.js";
+
+export class Negociacoes {
+    private negociacoes: Array<Negociacao> = [];
+
+    adiciona(negociacao:Negociacao){
+        this.negociacoes.push(negociacao);
+    }
+
+    lista(): ReadonlyArray<Negociacao>{
+        return this.negociacoes;
+    }
+
+
+}
